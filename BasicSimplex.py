a6294884ea2a6fc45423fbba99f2d0ea78fe0197
Krishna <krishnacharya97@gmail.com>
2019-10-29 20:57:30 +0100

    makes basic_in_row a bidict, helps in faster verbose modes and lookups

--- BasicSimplex.py
+++ BasicSimplex.py
@@ -1,6 +1,7 @@
 import numpy as np
 from fractions import Fraction
 import math
+from bidict import bidict
 ## Basic Tableau generator
 # Basic Simplex
 # Get objective function in OG Variables(x1,x2,...xn)
@@ -9,6 +10,8 @@ import math
 # Ratio test, Pivot
 # End when no more -ve variables in tableau
 # Or if there is a -ve variable and the ratio test gives no bound, unbounded.
+
+###TO IMPLEMENT - dictionary based basic_in_rows, will be faster for the verbose case
 
 def get_extra_vars(vec, ineq, b):
 	# input is numpy vector (contraints) the ineq and the constraint rhs
@@ -63,25 +66,44 @@ def parse_input(input_file):
 	return n, m, obj, abs(cst), updated_cst, ssa_cst
 
 
-def find_increaser(vector, pivot_rule = "max"):
+def find_increaser(vector, rule = "max"):
 	column = 0
-	if pivot_rule == "max":
+	if rule == "max":
 		column = max_coe(vector) # gives which x_c to choose.
-	if vector[column] >= 0:
-		return -1 #no -ve coeff found, therefore no more increase possible  
+	elif rule == "random": # randomly pick among the -ve elements
+		column = random_sel(vector)
+	elif rule == "bland":
+		column = bland_choice(vector)
+	else:
+		print("NO OTHER RULES")# possibly throw an error
+	if column == -1:
+		return -1 # NO MORE INCREASE
+	# if vector[column] >= 0:
+	#   return -1 #no -ve coeff found, therefore no more increase possible  
 	return column
 
 def max_coe(vector):
-	return np.argmin(vector[1:-1]) + 1 #gives which x_c to pivot on
+	# return np.argmin(vector[1:-1]) + 1 #gives which x_c to pivot on
+	col = np.argmin(vector[:-1])
+	if vector[col] >= 0:
+		return -1
+	return col
 
-def bland_choice():
-	pass
+def bland_choice(vector):
+	negval_indices = np.where(vector[:-1] < 0)[0]
+	if np.array_equal(negval_indices, []):
+		return -1
+	return negval_indices[0] # among all candidates for entering variable in BI, choose the one with smallest index
 
-def random_choice():
-	pass
+def random_sel(vector):
+	negval_indices = np.where(vector[:-1] < 0)[0]# a[0] is always 0, dummy val
+	if np.array_equal(negval_indices, []):
+		return -1
+	index = np.random.choice(len(negval_indices), 1) # pick one index
+	return negval_indices[index]
 
 def ratio_test_phase2(v1, v2):
-	# Yet to handle the degeneracy conditions, how to fixe --> pick row witht the lowest numbered variable in the basis
+	# YET TO HANDLE, pick row witht the lowest numbered variable in the basis
 	# ratios = v2[1:] / v1[1:]
 	v1 = v1[1:]; v2 = v2[1:]
 	# can be optimized
@@ -102,19 +124,19 @@ def ratio_test_phase2(v1, v2):
 			min_index = i
 			flag = 1
 		# if rhs/lhs >= 0:
-		# 	if ratios[i] < min_posval:
-		# 		min_posval = ratios[i]
-		# 		min_index = i
-		# 		flag = 1
+		#   if ratios[i] < min_posval:
+		#       min_posval = ratios[i]
+		#       min_index = i
+		#       flag = 1
 
 	if flag == 0: # no x_c < bound found
 		return -1
 	return min_index + 1
 
 
-def phase2(tableau, basic_in_row):
-	while(find_increaser(tableau[0]) != -1): # while there exists a non basic variable to increase along
-		col = find_increaser(tableau[0])
+def phase2(tableau, basic_in_row, rule = "max"):
+	while(find_increaser(tableau[0], rule) != -1): # while there exists a non basic variable to increase along
+		col = find_increaser(tableau[0], rule)
 		pivot_row = ratio_test_phase2(tableau[:,col], tableau[:,-1])
 		if pivot_row == -1:
 			return "UB"
@@ -132,11 +154,9 @@ def pivot(tableau, pivot_row, col, basic_in_row):
 		tableau[i] = tableau[i] - (tableau[i,col]) * tableau[pivot_row]
 	return tableau, basic_in_row
 
-
-
-def phase1(tableau, basic_in_row):
-	while(find_increaser(tableau[0]) != -1):
-		col = find_increaser(tableau[0])
+def phase1(tableau, basic_in_row, rule="max"):
+	while(find_increaser(tableau[0], rule) != -1):
+		col = find_increaser(tableau[0], rule)
 		pivot_row = ratio_test_phase2(tableau[:,col], tableau[:,-1])
 		tableau, basic_in_row = pivot(tableau, pivot_row, col, basic_in_row)
 	if tableau[0,-1] != 0:
@@ -145,14 +165,23 @@ def phase1(tableau, basic_in_row):
 
 def normalize(n, tableau, basic_in_row):
 	#normalization after phase1 gives feasible
-	for i in range(1,n+1):
-		row_pos = np.where(basic_in_row == i)[0]
-		if not np.array_equal(row_pos,[]):
-			tableau[0] = tableau[0] - tableau[0, i] * tableau[row_pos] ##
-	return tableau 
+	# this could case the issue
+	# for i in range(1,n+1):
+	# 	row_pos = np.where(basic_in_row == i)[0]
+	# 	if not np.array_equal(row_pos,[]):
+	# 		tableau[0] = tableau[0] - tableau[0, i] * tableau[row_pos] ##
+	# return tableau 
+	for i in range(1, n+1):
+		if i in basic_in_row.inverse:
+			tableau[0] = tableau[0] - tableau[0, i] * tableau[basic_in_row.inverse[i]]
+	return tableau
+
+def interpret_solution(tableau, basic_in_row):
+	pass
 
 
-def solve(input_file):
+# TO IMPLEMENT: also keep a variable to row mapping like x_1: rowc etc
+def solve(input_file, rule="max"):
 	n, m, obj, cst, updated_cst, ssa_cst = parse_input(input_file)
 	# print(n,m)
 	# print(obj, type(obj))
@@ -168,23 +197,27 @@ def solve(input_file):
 	  # b_vars = # all the slack
 	  tableau_direct[0,1:n+1] = -1 * obj#objective function
 	  for j in range(1,m+1):
-	      tableau_direct[j,1:n+1] = updated_cst[j-1]
-	      tableau_direct[j,n+j] = 1
-	      tableau_direct[j,-1] = cst[j-1]
-	  basic_in_row  = np.array(np.append([-2],[n+j for j in range(1,m+1)]),dtype=int) # basic variable associated with particular row
+		  tableau_direct[j,1:n+1] = updated_cst[j-1]
+		  tableau_direct[j,n+j] = 1
+		  tableau_direct[j,-1] = cst[j-1]
+	  basic_in_row = {j: n+j for j in range(1,m+1)}; basic_in_row[0] = -2
+	  basic_in_row = bidict(basic_in_row)
+	  #basic_in_row  = np.array(np.append([-2],[n+j for j in range(1,m+1)]),dtype=int) # basic variable associated with particular row
 	  # print(tableau_direct)
 	  # print(basic_in_row)
-	  Opval = phase2(tableau_direct, basic_in_row)
+	  Opval = phase2(tableau_direct, basic_in_row, rule)
 	  if Opval == "UB":
 	  	print("UNBOUNDED")
 	  else:
+	  	#interpret_solution(tableau_direct, basic_in_row)
 	  	print(Opval)
 	
 	else:
 		tot_extra = np.sum(ssa_cst)
-		tot_slsu = np.sum(ssa_cst[:,0:2]) # total number of slack and surplus vars	
+		tot_slsu = np.sum(ssa_cst[:,0:2]) # total number of slack and surplus vars  
 		tableau = np.zeros((m+1, n + tot_extra + 2), dtype = Fraction)
-		basic_in_row = np.zeros(m+1,dtype=int); basic_in_row[0] = -2 # some dummy value
+		basic_in_row = bidict({0:-2})
+		#basic_in_row = np.zeros(m+1,dtype=int); basic_in_row[0] = -2 # some dummy value
 		tableau[0, n+tot_slsu+1 : -1] = np.ones(tot_extra - tot_slsu, dtype=Fraction) # artifical vars
 		k = 0; l = 0 # shifters for the slack cols and artficial cols
 		for j in range(1, m+1):
@@ -206,7 +239,7 @@ def solve(input_file):
 				basic_in_row[j] = n+tot_slsu+l+1
 				tableau[0] = tableau[0] - tableau[j] # removing the artifical var from obj fun (only non basic in obj func)
 				l+=1
-		tableau, basic_in_row, status = phase1(tableau, basic_in_row)
+		tableau, basic_in_row, status = phase1(tableau, basic_in_row, rule)
 		if status == "infeasible": #also all the artificial vars must be basic
 			print("INFEASIBLE")
 		else:
@@ -214,10 +247,10 @@ def solve(input_file):
 			tableau[0, 1:n+1] = -1 * obj
 			tableau = np.delete(tableau,range(n+tot_slsu+1, n+tot_extra+1), axis=1) # must drop all the artificial variables
 			tableau = normalize(n, tableau, basic_in_row)
-			Opval = phase2(tableau, basic_in_row)
+			Opval = phase2(tableau, basic_in_row, rule)
 			if Opval == "UB":
 				print("UNBOUNDED")
 			else:
 				print(Opval)
 
-solve('km.txt')
+solve('knapsack.txt', rule="bland")
